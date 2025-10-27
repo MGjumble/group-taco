40cc54a8c3b9d05ff3845798b09ca1744f7c434d
Alexander Kolbfleisch <alexkolbfleisch@gmx.de>
2025-10-27 13:40:47 +0100

    [FPWGT-28] fix prettier problems

--- src/app/classes/json-petri-net.ts
+++ src/app/classes/json-petri-net.ts
@@ -1,15 +1,14 @@
 export interface JsonPetriNet {
-    places: string[],
-    transitions: string[],
-    arcs?: Record<string, number>,
-    actions?: string[],
-    labels?: Record<string, string>,
-    marking?: Record<string, number>,
-    layout?: Record<string, Coords | Coords[]>
+    places: string[];
+    transitions: string[];
+    arcs?: Record<string, number>;
+    actions?: string[];
+    labels?: Record<string, string>;
+    marking?: Record<string, number>;
+    layout?: Record<string, Coords | Coords[]>;
 }
 
 export interface Coords {
-    x: number,
-    y: number
+    x: number;
+    y: number;
 }
-
