615bb1253bd78a10a4774590f9172ef00dff8ac1
Marie Gabrielsen <m.gabrielsen@outlook.de>
2025-11-08 22:56:57 +0100

    Add provideHttpClient/ -Testing to providers

--- src/app/components/tab-toolbar/play/firing-table/firing-table.component.spec.ts
+++ src/app/components/tab-toolbar/play/firing-table/firing-table.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { FiringTableComponent } from './firing-table.component';
 
@@ -8,7 +10,11 @@ describe('FiringTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FiringTableComponent]
+      imports: [FiringTableComponent],
+      providers: [
+          provideHttpClient(),
+          provideHttpClientTesting(),
+      ],
     })
     .compileComponents();
 
