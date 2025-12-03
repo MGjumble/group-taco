0f71d09a41bbe643d5be32e4d5e36ffb2d2d9324
fsm-fuh <fsmerkel@gmail.com>
2025-12-03 19:53:11 +0100

    Zwischenstand, 2025-12-03_a

--- src/app/reachability-graph.service.ts
+++ src/app/reachability-graph.service.ts
@@ -7,5 +7,9 @@ export class ReachabilityGraphService {
   
 //bekommt firing entry und macht dann eine nodeID daraus und übergibt an reachability graph als stateNode+//woher x und y?
 
+get marking
+only take numbers from record of Sting and number, display as label and add to model (Id)
+
+
 
 }
