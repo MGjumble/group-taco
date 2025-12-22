91a348fa8c72640f2e9d29dde61329411499bdb6
MGjumble <m.gabrielsen@outlook.de>
2025-12-22 19:08:16 +0100

    [FPWGT-31] Extend firing table features and appearance, reset marking for process net and draw tab (#25)
    * [FPWGT-31] Disable playing in Draw/ PN tabs
    
    A click on an svg-node is now handled in the DisplayComponent and only
    forwarded to the PlayService if the current tab is Play or Reachability
    Graph.
    The DiagramTransition methods 'isActivated' and 'fire' have been made
    public so that the PlayService can call them.
    
    * [FPWGT-31] Fix linting
    
    * [FPWGT-50] Reset firing entries on net clear
    
    * [FPWGT-47] Show start/ end marking in firing entry
    
    and update marking in diagram after firing
    
    * [FPWGT-48] Delimit firing sequences with spaces
    
    * [FPWGT-31] Remove spaces from marking display
    
    * [FPWGT-50] Relocate resetFiringEntries call
    
    * [FPWGT-55] Add start marking to Diagram
    
    * [FPWGT-55] Add reset marking function to Diagram
    
    * [FPWGT-31] Remove unused imports
    
    * [FPWGT-31] Update source net after firing a transition
    
    * [FPWGT-31] Add comments
    
    * [FPWGT-31] Remove unused import
    
    * [FPWGT-31] Improve subscription mechanism
    
    * [FPWGT-31] Implement OnInit & OnDestroy in PlayCo.
    
    * [FPWGT-57] Add delete buttons to clear f. entries
    
    * [FPWGT-60] Rename new firing entry function
    
    * [FPWGT-65] Adjust styling of the firing table
    
    * [FPWGT-31] Initially display no rows in firing t.
    
    * [FPWGT-31] Reset PlayService's attributes on...
    
    ...net cleared
    
    * [FPWGT-31] Apply prettier codestyle
    
    * [FPWGT-66] Turn firingEntry into a class to...
    
    ...be able to have methods for formatted markings
    
    * [FPWGT-66] Move formatMarking logic to FiringEntry
    
    * [FPWGT-66] Adjust play service to Fir.Entry class
    
    * [FPWGT-31] Use absolute import path
    
    * [FPWGT-31] Delete unecessary import
    
    * [FPWGT-66] Add input fields in active firing entry
    
    * [FPWGT-66] Add focus to firing sequence input on...
    
    ...new entry addition. Show activated transitions in reachability graph
    tab.
    
    * [FPWGT-31] Rename method checking if transition...
    
    ...can be fired, because the check depends on the current tab and state,
    not on the type 'transition' (e. g. a node may be a transition in the
    drawing tab but cannot be fired there).
    
    * [FPWGT-31] Add docstrings
    
    * [FPWGT-31] Remove unused play service
    
    * [FPWGT-31] Add 'take' import to play component
    
    * [FPWGT-65] Adjust firing table styles
    
    * [FPWGT-31] Remove comment
    
    * [FPWGT-31] Include resetMarking in methods of...
    
    ...play service that start new firing sequences
    
    * [FPWGT-82] Reset marking when process net tab...
    
    ...is entered and recover last marking on return to play tab or
    reachability graph tab.
    
    * [FPWGT-31] Apply prettier cody style
    
    * [FPWGT-82] Rename function to recover last marking
    
    * [FPWGT-82] Reset marking when switching to Draw
    
    * [FPWGT-31] Remove trivial type annotation
    
    * [FPWGT-31] Replace let with const
    
    * add plus and minus icons to firing table for better user experience and easier validation later on
    
    ---------
    
    Co-authored-by: Alexander Kolbfleisch <alexkolbfleisch@gmx.de>
    Co-authored-by: Alex <92081744+alexkolbfleisch@users.noreply.github.com>

--- src/app/services/play.service.ts
+++ src/app/services/play.service.ts
@@ -17,6 +17,9 @@ export class PlayService {
 
     private _startMarking: Record<string, number> = {};
     private _currentMarking = signal<Record<string, number>>(this._startMarking);
+    private _lastMarking: Record<string, number> | undefined = undefined;
+    private _idCounter = 0;
+
     firingEntries = signal<FiringEntry[]>([]);
 
     set startMarking(marking: Record<string, number>) {
@@ -28,10 +31,20 @@ export class PlayService {
     }
 
     /**
-     * Clears all firing entries in the firing sequence table.
+     * Recovers the marking of the diagram from the last marking stored in the service.
+     * @param diagram
+     *          The diagram to recover the marking for.
+     */
+    recoverLastMarking(diagram: Diagram): void {
+        if (this._lastMarking) diagram.marking = this._lastMarking;
+    }
+
+    /**
+     * Clears all firing entries in the firing sequence table and deletes the last marking.
      */
     resetFiringEntries(): void {
         this.firingEntries.set([]);
+        this._lastMarking = undefined;
     }
 
     /**
@@ -45,6 +58,7 @@ export class PlayService {
         if (node.isActivated()) {
             node.fire();
             diagram.updateMarking();
+            this._lastMarking = diagram.marking;
             this._sourceNetService.updateEditedNet(diagram);
             this._addTransitionToFiringSequence(node.label);
         } else
@@ -62,7 +76,8 @@ export class PlayService {
     /**
      * Updates the current firing entry when a transition is fired.
      * If no entry exists, creates a new one.
-     * @param label The label of the fired transition.
+     * @param label
+     *          The label of the fired transition.
      */
     private _addTransitionToFiringSequence(label: string): void {
         this.firingEntries.update((entries) => {
@@ -87,8 +102,10 @@ export class PlayService {
     /**
      * Appends the label of a fired transition to a firing sequence
      * and updates transition count and end marking accordingly.
-     * @param entry The entry to be updated.
-     * @param label The label of the fired transition.
+     * @param entry
+     *          The entry to be updated.
+     * @param label
+     *          The label of the fired transition.
      * @returns The updated firing entry.
      */
     private _updateFiringEntry(entry: FiringEntry, label: string): FiringEntry {
@@ -96,5 +113,32 @@ export class PlayService {
         entry.transitionCount += 1;
         entry.endMarking = this._currentMarking();
         return entry;
+    }
+
+    /**
+     * Closes the last firing entry in the firing table, preventing further updates to it.
+     */
+    private _closeLastFiringEntry(): void {
+        this.firingEntries.update((entries) => {
+            const lastEntry = entries[entries.length - 1];
+            if (lastEntry && !lastEntry.isClosed) lastEntry.isClosed = true;
+            return entries;
+        });
+    }
+
+    /**
+     * Creates a new empty firing entry with start values.
+     * @returns A firing entry with an empty sequence
+     */
+    private _getEmptyFiringEntry(): FiringEntry {
+        return new FiringEntry(this._getNewId(), '', 0, this._startMarking, this._startMarking, false);
+    }
+
+    /**
+     * Generates a new unique ID for a firing entry.
+     * @returns The new ID
+     */
+    private _getNewId(): number {
+        return this._idCounter++;
     }
 }
